876e1325397e5a5eba16627fc913492f530d960a
jaylyn battistini <jaylynbattistini@jaylyns-MacBook-Air.local>
2022-03-29 16:47:39 -0400

    Upload Test

---
+++ Assignment/Group Selector/ContentViewModel.swift
@@ -0,0 +1,74 @@
+//
+//  ContentViewModel.swift
+//  Group Selector
+//
+//  Created by Adrian Devezin on 11/9/21.
+//
+
+import Foundation
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    // TODO: Create an array of strings named allClassmates that holds all classmates names
+    var allClassmates = [ "Kayla", "Lauryn","Thomas", "Angel", "Dawit","Joshua", "Cam", "Javin", "Jaylyn"]
+    @Published
+    var groups: String = ""
+
+    /**
+     * Splits all classmates into groups of three, and updates the groups variable
+     */
+    func createGroups() {
+        // TODO: Create a mutable array of strings named classmates that is equal to allClassmates
+        var classmates = allClassmates
+        // TODO: Create a mutable string named result that is equal to an empty string
+        var result: String = ""
+        // TODO: Create a mutable int named count that is equal to 1
+        var count: Int = 1
+        
+        while !classmates.isEmpty {
+            if !result.isEmpty {
+                result +=  "\n"
+            }
+            
+            let groupNumber = "Group \(count): "
+            //TODO: Add groupNumber to result
+            result = result + groupNumber
+            let studentsPicked = pick3(classmates)
+            classmates -= studentsPicked
+            let studentsPickedString = studentsPicked.joined(separator: ", ")         //TODO: add studentsPickedString to result
+            result = result + studentsPickedString
+            //TODO: add one to count
+            count = count + 1
+        }
+        self.groups = result
+
+    }
+    
+    /**
+     * Picks up to 3 random elements from an array
+     */
+    private func pick3(_ remainingClassmates: [String]) -> [String] {
+        let max = 3 > remainingClassmates.count ? remainingClassmates.count - 1 : 2
+        return Array(remainingClassmates.shuffled()[...max])
+    }
+}
+
+
+
+extension Array where Element: Equatable {
+
+    static func -=(lhs: inout Array, rhs: Array) {
+
+        rhs.forEach {
+            if let indexOfhit = lhs.firstIndex(of: $0) {
+                lhs.remove(at: indexOfhit)
+            }
+        }
+    }
+
+    static func -(lhs: Array, rhs: Array) -> Array {
+
+        return lhs.filter { return !rhs.contains($0) }
+    }
+}
